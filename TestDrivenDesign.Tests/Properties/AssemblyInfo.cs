8d30a081e2d22cd0d0a34b21d7f033aced3e04c9
Jacob Foshee <jacobf@gmail.com>
2011-06-18 12:45:57 -0500

    Updated project files with my usual changes.  Turned on all warnings and code analysis in release build.

--- TestDrivenDesign.Tests/Properties/AssemblyInfo.cs
+++ TestDrivenDesign.Tests/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestDrivenDesign.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Jacob Foshee")]
 [assembly: AssemblyProduct("TestDrivenDesign.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Jacob Foshee 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- TestDrivenDesign/Properties/AssemblyInfo.cs
+++ TestDrivenDesign/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("TestDrivenDesign")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("Jacob Foshee")]
 [assembly: AssemblyProduct("TestDrivenDesign")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("Copyright © Jacob Foshee 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
