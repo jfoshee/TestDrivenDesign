ecb82068dad0ee1204bc86b40a7654fb38b9039b
Jacob Foshee <jacobf@gmail.com>
2011-06-22 07:21:25 -0500

    Added a project for examples and a first example

---
+++ TestDrivenDesign.Examples/HelloPlainTextTest.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDrivenDesign.Examples
+{
+    class HelloPlainText
+    {
+        internal static void Write(string path)
+        {
+            File.WriteAllText(path, "Hello, World!");
+        }
+    }
+
+    [TestClass]
+    public class HelloPlainTextTest : TestBase
+    {
+        [TestMethod]
+        public void ShouldWriteHelloToFile()
+        {
+            // Arrange a unique file path for this test
+            var path = TestPath() + ".txt";
+
+            // Act
+            HelloPlainText.Write(path);
+
+            // Assert the file contains the word 'Hello' 
+            // and add a link to the results page for convenient human inspection
+            TestContext.AddResultFile(path);
+            var text = File.ReadAllText(path);
+            StringAssert.Contains(text, "Hello");
+        }
+    }
+}
--- readme.markdown
+++ readme.markdown
@@ -7,15 +7,10 @@ Inheriting from the TestBase class will provide a TestContext property.
 The TestContext property is useful for tests involving file I/O and for finding information about the current test run.
 See http://msdn.microsoft.com/en-us/library/ms404699.aspx 
 
-	using Microsoft.VisualStudio.TestTools.UnitTesting;
-	using TestDrivenDesign;
-
-	namespace TestDrivenDesign.Tests
-	{
 		[TestClass]
 		public class MyExampleTest : TestBase
-		{
-		.
-		.
-		.
 
+Inheriting from the typed TestBase<T> class will provide a test Subject.
+
+		[TestClass]
+		public class MyExampleTest : TestBase<MyExample>
