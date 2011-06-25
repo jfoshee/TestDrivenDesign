9142f480eef7f961d7f6b42578f7d038dd6958e5
Jacob Foshee <jacobf@gmail.com>
2011-06-25 17:05:55 -0500

    Added BinaryFileAssert, including Exists() and StartsWith()

---
+++ TestDrivenDesign.Tests/BinaryFileAssertTest.cs
@@ -0,0 +1,53 @@
+﻿using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestDrivenDesign.Tests
+{
+    [TestClass]
+    public class BinaryFileAssertTest : TestBase
+    {
+        [TestMethod, ExpectedException(typeof(AssertFailedException))]
+        public void FileDoesNotExist()
+        {
+            BinaryFileAssert.Exists(TestPath());
+        }
+
+        [TestMethod]
+        public void FileExists()
+        {
+            // Arrange
+            var path = TestPath();
+            File.WriteAllBytes(path, new byte[] { 0 });
+
+            // Act
+            BinaryFileAssert.Exists(path);
+        }
+
+        [TestMethod, ExpectedException(typeof(AssertFailedException))]
+        public void DoesNotStartWith()
+        {
+            // Arrange
+            var path = WriteBinaryTenToFifty();
+
+            // Act
+            BinaryFileAssert.StartsWith(path, new byte[] { 30, 40, 50 });
+        }
+
+        [TestMethod]
+        public void DoesStartWith()
+        {
+            // Arrange
+            var path = WriteBinaryTenToFifty();
+
+            // Act
+            BinaryFileAssert.StartsWith(path, new byte[] { 10, 20, 30 });
+        }
+
+        private string WriteBinaryTenToFifty()
+        {
+            var path = TestPath();
+            File.WriteAllBytes(path, new byte[] { 10, 20, 30, 40, 50 });
+            return path;
+        }
+    }
+}
---
+++ TestDrivenDesign/BinaryFileAssert.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System.Linq;
+
+namespace TestDrivenDesign
+{
+    public static class BinaryFileAssert
+    {
+        public static void Exists(string path)
+        {
+            TextFileAssert.Exists(path);
+        }
+
+        public static void StartsWith(string path, byte[] expected)
+        {
+            using (var reader = new BinaryReader(File.OpenRead(path)))
+            {
+                var actual = reader.ReadBytes(expected.Length);
+                if (!Enumerable.SequenceEqual(expected, actual))
+                    throw new AssertFailedException("File does not start with expected bytes");
+            }
+        }
+    }
+}
