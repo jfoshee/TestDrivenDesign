83ff5e4a015bef5643c67e759b6642bdb55b0c70
Jacob Foshee <jacobf@gmail.com>
2011-08-04 16:01:07 -0500

    Added DirectoryAssert.Count and DirectoryAssert.DoesNotContain via github

--- TestDrivenDesign/DirectoryAssert.cs
+++ TestDrivenDesign/DirectoryAssert.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -14,7 +15,7 @@ namespace TestDrivenDesign
 
         public static void Contains(string directory, string searchPattern)
         {
-            if (Directory.EnumerateFileSystemEntries(directory, searchPattern).Count() == 0)
+            if (GetCount(directory, searchPattern) == 0)
                 throw new AssertFailedException("Directory does not contain: " + searchPattern);
         }
 
@@ -22,6 +23,29 @@ namespace TestDrivenDesign
         {
             if (Directory.EnumerateFiles(directory).Count() > 0)
                 throw new AssertFailedException("Directory is not empty: " + directory);
+        }
+
+        public static void DoesNotContain(string directory, string searchPattern)
+        {
+            if (GetCount(directory, searchPattern) != 0)
+                throw new AssertFailedException("Directory does contain: " + searchPattern);
+        }
+
+        public static void Count(string directory, int expected)
+        {
+            Count(directory, "*", expected);
+        }
+
+        public static void Count(string directory, string searchPattern, int expected)
+        {
+            var count = GetCount(directory, searchPattern);
+            if (count != expected)
+                throw new AssertFailedException(String.Format("Directory contains {0} file entries matching '{1}'.  Expected: {2}", count, searchPattern, expected));
+        }
+
+        private static int GetCount(string directory, string searchPattern)
+        {
+            return Directory.EnumerateFileSystemEntries(directory, searchPattern).Count();
         }
     }
 }
