ab7744a64153afc302eb9a563b72e68a31af3a2b
jfoshee <jacobf@gmail.com>
2011-08-12 15:33:05 -0500

    Added a nuspec file to generate a NuGet package, and pushed package to NuGet Gallery. From project directory: NuGet spec NuGet pack TestDrivenDesign.csproj -Prop Configuration=Release NuGet Push TestDrivenDesign.0.0.1.nupkg

--- TestDrivenDesign/Properties/AssemblyInfo.cs
+++ TestDrivenDesign/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TestDrivenDesign")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Augments Microsoft's Visual Studio unit testing framework and helps out with TDD")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Jacob Foshee")]
 [assembly: AssemblyProduct("TestDrivenDesign")]
@@ -31,6 +31,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
 [assembly: CLSCompliant(true)]
